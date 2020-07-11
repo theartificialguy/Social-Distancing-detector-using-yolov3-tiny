42a8302fc4cbe618c087edbf3d815990f35bb7b0
yash-007 <46284422+yash-007@users.noreply.github.com>
2020-07-11 21:52:59 +0530

    social distancing detection

--- social_distance_yolov3_tiny.py
+++ social_distance_yolov3_tiny.py
@@ -3,6 +3,7 @@ import numpy as np
 import random
 import os
 import time
+from scipy.spatial import distance as dist
 
 net = cv2.dnn.readNet("yolov3-tiny.weights", "yolov3-tiny-custom-social.cfg")
 
@@ -11,17 +12,12 @@ distance_threshold = 50
 cap = cv2.VideoCapture('humans.mp4')
 
 fourcc = cv2.VideoWriter_fourcc(*'XVID')
-out = cv2.VideoWriter('output.avi', fourcc, 20.0, ((int(cap.get(3)), int(cap.get(4)))))
-
-def dist(pt1,pt2):
-    try:
-        return ((pt1[0]-pt2[0])**2 + (pt1[1]-pt2[1])**2)**0.5
-    except:
-        return
+out = cv2.VideoWriter('output.avi', fourcc, 20.0, ((int(cap.get(3)), int(cap.get(4))))
 
 layer_names = net.getLayerNames()
 output_layers = [layer_names[i[0] - 1] for i in net.getUnconnectedOutLayers()]
  
+MIN_DISTANCE = 50
 ret = True
 
 while ret:
@@ -56,36 +52,34 @@ while ret:
                 boxes.append([x, y, w, h])
                 confidences.append(float(confidence))
 
-    indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.5, 0.4)
-
-    persons = []
-    centroids = []
-    violate = set()
-
-    for i in range(len(boxes)):
-        if i in indexes:
-            x,y,w,h = boxes[i]
-            persons.append(boxes[i])
-            centroids.append([x+w//2, y+h//2])
-
-    for i in range(len(persons)):
-        for j in range(i+1, len(persons)):
-            if dist(centroids[i], centroids[j]) <= distance_threshold:
-                violate.add(tuple(persons[i]))
-                violate.add(tuple(persons[j]))
+    results = cv2.dnn.NMSBoxes(boxes, confidences, 0.5, 0.4)
     
-    v = 0
-    for (x,y,w,h) in persons:
-        if (x,y,w,h) in violate:
-            color = (0,0,255)
-            v+=1
-        else:
-            color = (0,255,0)
-        cv2.rectangle(img, (x,y), (x+w,y+h), color, 2)
-        cv2.circle(img, (x+w//2,y+h//2), 2, (0,0,255), 2)
+    violate = set()
+	if len(results) >= 2:
+		centroids = np.array([r[2] for r in results])
+		D = dist.cdist(centroids, centroids, metric="euclidean")
 
-    cv2.putText(img,'No of Violations : '+str(v), (20,350), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255), 1)
+		for i in range(0, D.shape[0]):
+			for j in range(i + 1, D.shape[1]):
+				if D[i, j] < MIN_DISTANCE:
+					violate.add(i)
+					violate.add(j)
 
+	for (i, (prob, bbox, centroid)) in enumerate(results):
+		(startX, startY, endX, endY) = bbox
+		(cX, cY) = centroid
+		color = (0, 255, 0)
+
+		if i in violate:
+			color = (0, 0, 255)
+
+		cv2.rectangle(img, (startX, startY), (endX, endY), color, 2)
+		cv2.circle(img, (cX, cY), 5, color, 1)
+                      
+	txt = "Social Distancing Violations Detected: {}".format(len(violate))
+	cv2.putText(img, txt, (10, img.shape[0] - 25), cv2.FONT_HERSHEY_SIMPLEX, 0.85, (0, 0, 255), 3)
+
+    
     frame = cv2.flip(img, 0)
     out.write(frame)
 
